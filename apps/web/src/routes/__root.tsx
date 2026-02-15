4bd79e16612ee905a69121b677c4f4dddb2ee913
marromugi <marrromugi@gmail.com>
2026-02-15 22:34:11 +0900

    fix build error

--- apps/web/src/routes/__root.tsx
+++ apps/web/src/routes/__root.tsx
@@ -1,16 +1,6 @@
 import { ToastProvider } from '@ding/ui'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
-import { lazy, Suspense } from 'react'
-
-// import.meta.env.PROD
-const TanStackRouterDevtools =
-  1 === Number(1)
-    ? () => null
-    : lazy(() =>
-        import('@tanstack/react-router-devtools').then((res) => ({
-          default: res.TanStackRouterDevtools,
-        }))
-      )
+import { Suspense } from 'react'
 
 export const Route = createRootRoute({
   component: RootComponent,
